27fdbe68ec7f446ea36321ea0e813a12ea631454
luqmanabyadh <lqmn.abydh@gmail.com>
2020-02-27 14:33:57 +0700

    exercise4 done

---
+++ exercise4.js
@@ -0,0 +1,44 @@
+var hari = '15'
+var bulan = 7
+var tahun = '1995'
+
+switch (bulan) {
+    case 1:
+        var bulan = 'Januari'
+        break;
+    case 2:
+        var bulan = 'Februari'
+        break;
+    case 3:
+        var bulan = 'Maret'
+        break;
+    case 4:
+        var bulan = 'April'
+        break;
+    case 5:
+        var bulan = 'Mei'
+        break;
+    case 6:
+        var bulan = 'Juni'
+        break;
+    case 7:
+        var bulan = 'Juli'
+        break;
+    case 8:
+        var bulan = 'Agustus'
+        break;
+    case 9:
+        var bulan = 'September'
+        break;
+    case 10:
+        var bulan = 'Oktober'
+        break;
+    case 11:
+        var bulan = 'November'
+        break;
+    case 12:
+        var bulan = 'Desember'
+        break;
+}
+
+console.log(hari + ' ' + bulan + ' ' + tahun)
