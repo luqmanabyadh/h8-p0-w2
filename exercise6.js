8b11dac067c265e5cec13ac5eb95429d7cb9d702
luqmanabyadh <lqmn.abydh@gmail.com>
2020-02-27 16:49:07 +0700

    yal bisa

---
+++ exercise6.js
@@ -0,0 +1,32 @@
+// No.1 (looping dengan while)
+
+var looping = 2
+var looping2 = 20
+
+console.log ('LOOPING PERTAMA')
+while (looping < 21) {
+    console.log(looping + ' ' + '- I love coding')
+    looping+=2
+}
+
+console.log('LOOPING KEDUA')
+while (looping2 > 0) {
+    console.log(looping2 + ' ' + '- I will become fullstack developer')
+    looping2-=2
+}
+
+// No.2 (looping dengan for)
+
+console.log('LOOPING PERTAMA')
+for(var looping3 = 1; looping3 < 21; looping3++) {
+    console.log(looping3 + ' ' + '- I love coding')
+}
+
+console.log('LOOPING KEDUA')
+for(var looping4 = 20; looping4 > 0; looping4--) {
+    console.log(looping4 + ' ' + '- I will become fullstack developer')
+}
+
+// No.3 (looping ganjil genap)
+
+
--- tes.js
+++ tes.js
@@ -1,18 +1,10 @@
-var word4 = 'wow Javasript is so cool'
-var exampleFirstWord4 = word4.substring(0, 3)
-var secondWord4 = word4.substring(4, 13)
-var thirdWord4 = word4.substring(14, 16)
-var fourthWord4 = word4.substring(17, 19)
-var fifthWord4 = word4.substring(20)
 
-var firstWordLength = exampleFirstWord4.length
-var secondWordLength = secondWord4.length
-var thirdWordLength = thirdWord4.length
-var fourthWordLength = fourthWord4.length
-var fifthWordLength = fifthWord4.length
 
-console.log('First Word:' + exampleFirstWord4 + ', with length: ' + firstWordLength)
-console.log('Second Word:' + secondWord4 + ', with length: ' + secondWordLength)
-console.log('Third Word:' + thirdWord4 + ', with length: ' + thirdWordLength)
-console.log('Fourth Word:' + fourthWord4 + ', with length: ' + fourthWordLength)
-console.log('Fifth Word:' + fifthWord4 + ', with length: ' + fifthWordLength)
+
+// var jumlah = 0;
+// for(var deret = 5; deret > 0; deret--) {
+//   jumlah += deret;
+//   console.log('Jumlah saat ini: ' + jumlah);
+// }
+
+// console.log('Jumlah terakhir: ' + jumlah);
